53db0de76f491ad7e8b5f9b7c5f86d3151775cdc
andyx0 <77806295+andyx0@users.noreply.github.com>
2022-08-01 10:05:23 -0400

    Add files via upload

---
+++ Labeled_Case/Labeled_Experiment_Multi_Layer.py
@@ -0,0 +1,178 @@
+from util import generate_instance, construct_DG
+from show_arrangement import show_arrangement
+
+import math
+import networkx as nx
+import matplotlib.pyplot as plt
+import gurobipy as grb
+
+
+def Labeled_Experiment(nx_graph, numObjs, Density, Height=1000, Width=1000, layers=1):
+    '''
+    Args:
+    numObjs: number of objects in this problem
+    Density: S(occupied area)/S(environment)
+    Height, Width: Environment size
+    '''
+    # disc radius
+    RAD = math.sqrt((Height*Width*Density)/(math.pi*numObjs))
+
+    # Load instance (start arrangement, goal arrangement)
+    start_arr, goal_arr = generate_instance(numObjs, Density)
+
+    # Generate the dependency graph
+    Digraph = construct_DG(start_arr, goal_arr, RAD, layers)
+    nx_graph = nx.DiGraph(Digraph)
+    action_sequence = optimal_sequence(nx_graph)
+    for action in action_sequence:
+        print(action)
+    # show_Digraph(nx_graph, "Final Graph")
+
+    # show instance
+    show_arrangement(numObjs, Density, start_arr, goal_arr)
+
+
+def optimal_sequence(Digraph):
+    action_sequence = []
+    show_digraph(Digraph, "Original Graph")
+    edges = Digraph.edges(data=True)
+    layer_edges = list(x for x in edges if 'layer' in x[-1])
+    layer_graph = nx.DiGraph()
+    layer_graph.add_nodes_from(Digraph)
+    layer_graph.add_edges_from(layer_edges)
+    show_digraph(layer_graph, "Layer Graph")
+
+    # Use Tarjan's SCC decomposition algo to find SCCs in reverse topological order
+    partition = list(nx.strongly_connected_components(Digraph))
+
+    for SCC in partition:
+        SCC_list = list(SCC)
+        if len(SCC_list) == 1:
+            action_sequence.append((SCC_list[0], 'g'))
+        if len(SCC_list) > 1:
+            print("SCC (" + str(len(SCC_list)) + "): " + str(SCC_list))
+            new_Graph = Digraph.subgraph(SCC_list).copy()  # construct the strongly connected component
+            mfvs = ILP_MFVS(new_Graph)
+            show_digraph(new_Graph, "Strongly Connected Component")
+            # subgraph of only weighted edges
+            mfvs_graph = layer_graph.subgraph(mfvs)
+            mfvs = list(reversed(list(nx.topological_sort(mfvs_graph))))
+            for v in mfvs:
+                action_sequence.append((v, 'b'))
+                new_Graph.remove_node(v)
+            if nx.is_directed_acyclic_graph(new_Graph):
+                rev_list = list(reversed(list(nx.topological_sort(new_Graph))))
+                print("Reverse Topological (without MFVS): " + str(rev_list))
+                for item in rev_list:
+                    action_sequence.append((item, 'g'))
+            else:
+                print("Not a DAG!")
+            for v in mfvs:
+                action_sequence.append((v, 'g'))
+    return action_sequence
+
+
+def ILP_MFVS(subgraph):
+    env = grb.Env(empty=True)
+    env.setParam("OutputFlag", 0)
+    env.start()
+    model = grb.Model("mip1", env=env)
+    for x in subgraph.nodes:
+        model.addVar(vtype=grb.GRB.BINARY, name=str(x))
+    model.update()
+    model.setObjective(grb.quicksum(model.getVars()), grb.GRB.MAXIMIZE)
+    for cycle in nx.simple_cycles(subgraph):
+        model.addConstr((grb.quicksum(model.getVarByName(str(graphNode)) for graphNode in cycle)) <= len(cycle) - 1)
+    edges = subgraph.edges(data='layer')
+    for edge in edges:
+        is_layer_edge = edge[-1]
+        if is_layer_edge:
+            sourceNode = str(edge[0])
+            targetNode = str(edge[1])
+            model.addConstr(model.getVarByName(sourceNode) >= model.getVarByName(targetNode))
+
+    model.optimize()
+    mfvs = set()
+    for v in model.getVars():
+        if not v.x:
+            mfvs.add(int(v.varName))
+    print("Gurobi ILP MFVS: " + str(mfvs))
+    return mfvs
+
+
+def show_digraph(Digraph, window_title=''):
+    pos = nx.spring_layout(Digraph)
+    nx.draw_networkx(Digraph, pos)
+    labels = nx.get_edge_attributes(Digraph, 'layer')
+    nx.draw_networkx_edge_labels(Digraph, pos, edge_labels=labels)
+    if window_title:
+        plt.get_current_fig_manager().set_window_title(window_title)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # test 1 (no cycles)
+    graph_1 = nx.DiGraph()
+    graph_1.add_edge(2, 1, layer=True)
+    graph_1.add_edge(3, 4, layer=True)
+    graph_1.add_edge(1, 0)
+    graph_1.add_edge(2, 3)
+    # test 2 (cycles)
+    graph_2 = nx.DiGraph()
+    graph_2.add_edge(0, 1, layer=True)
+    graph_2.add_edge(2, 3, layer=True)
+    graph_2.add_edge(3, 0)
+    graph_2.add_edge(1, 2)
+    graph_2.add_edge(0, 2)
+    graph_2.add_edge(2, 0)
+    # test 2.5 (cycles)
+    graph_2_5 = nx.DiGraph()
+    graph_2_5.add_edge(0, 1, layer=True)
+    graph_2_5.add_edge(2, 3, layer=True)
+    graph_2_5.add_edge(3, 2)
+    graph_2_5.add_edge(1, 2)
+    graph_2_5.add_edge(0, 2)
+    graph_2_5.add_edge(2, 0)
+    # test 3 (cycles)
+    graph_3 = nx.DiGraph()
+    graph_3.add_edge(0, 1, layer=True)
+    graph_3.add_edge(1, 2)
+    graph_3.add_edge(0, 2)
+    graph_3.add_edge(2, 0)
+    # test 4 (cycles)
+    graph_4 = nx.DiGraph()
+    graph_4.add_edge(0, 1, layer=True)
+    graph_4.add_edge(2, 4, layer=True)
+    graph_4.add_edge(1, 2)
+    graph_4.add_edge(4, 2)
+    graph_4.add_edge(0, 3)
+    graph_4.add_edge(2, 3)
+    graph_4.add_edge(3, 0)
+    # test 4_5 (cycles with 3 layers)
+    graph_4_5 = nx.DiGraph()
+    graph_4_5.add_edge(0, 1, layer=True)
+    graph_4_5.add_edge(1, 2, layer=True)
+    graph_4_5.add_edge(2, 3, layer=True)
+    graph_4_5.add_edge(1, 0)
+    graph_4_5.add_edge(3, 0)
+    # test 5 (cycles)
+    graph_5 = nx.DiGraph()
+    graph_5.add_edge(4, 1, layer=True)
+    graph_5.add_edge(4, 0)
+    graph_5.add_edge(0, 4)
+    graph_5.add_edge(4, 2)
+    graph_5.add_edge(2, 4)
+    graph_5.add_edge(4, 3)
+    graph_5.add_edge(3, 4)
+    # test 6 (demo)
+    graph_6 = nx.DiGraph()
+    graph_6.add_edge(1, 4, layer=True)
+    graph_6.add_edge(2, 4, layer=True)
+    graph_6.add_edge(4, 3, layer=True)
+    graph_6.add_edge(3, 5)
+    # Labeled_Experiment(graph_1, 100, 0.4)
+    # Labeled_Experiment(graph_2, 30, 0.4)
+    # Labeled_Experiment(graph_2_5, 30, 0.4)
+    # Labeled_Experiment(graph_3, 30, 0.4)
+    # Labeled_Experiment(graph_4, 30, 0.4)
+    Labeled_Experiment(graph_6, 120, 0.4, layers=2)
--- Labeled_Case/util.py
+++ Labeled_Case/util.py
@@ -5,12 +5,19 @@ import math
 import json
 
 
+def constrained_sum_sample(n, total):
+    """Return a randomly chosen list of n positive integers summing to total.
+    Each such list is equally likely to occur."""
+
+    dividers = sorted(random.sample(range(1, total), n - 1))
+    return sorted([a - b for a, b in zip(dividers + [total], [0] + dividers)], reverse=True)
+
+
 def generate_instance(numObjs, Density):
     '''
     Args:
     numObjs: number of objects in this problem
     Density: S(occupied area)/S(environment)
-
     Returns:
     start_arr: start arrangement. key: int obj ID, value: (x,y) coordinate
     goal_arr: goal arrangement. key: int obj ID, value: (x,y) coordinate
@@ -56,22 +63,57 @@ def generate_instance(numObjs, Density):
     return start_arr, goal_arr
 
 
-def construct_DG(start_arr, goal_arr, radius):
+def construct_DG(start_arr, goal_arr, radius, layers=1):
     '''
     Args:
     start_arr: start arrangement. key: int obj ID, value: (x,y) coordinate
     goal_arr: goal arrangement. key: int obj ID, value: (x,y) coordinate
     radius: disc radius
-
     Returns:
     dependency graph DG: DG[objID]=set(obj1, obj2, ...)
     '''
+    layer_arr = []
+    if layers > 1:
+        layer_size_arr = constrained_sum_sample(layers, len(start_arr))
+        layer_index_arr = layer_size_arr.copy()
+        for i in range(1, len(layer_index_arr)):
+            layer_index_arr[i] = layer_index_arr[i - 1] + layer_index_arr[i]
+        # print("layer_size_arr: " + str(layer_size_arr))
+        # print("layer_index_arr: " + str(layer_index_arr))
+        # create each layer one at a time, assign layer dependencies, and set coords
+        base_layer_obj_id = 0
+        curr_layer_obj_id = layer_index_arr[0]
+        for layer in range(1, layers):
+            # print("layer: " + str(layer))
+            # print("base_layer_obj_id: " + str(base_layer_obj_id))
+            # print("curr_layer_obj_id: " + str(curr_layer_obj_id))
+            for i in range(layer_size_arr[layer]):
+                start_arr[curr_layer_obj_id + i] = start_arr[base_layer_obj_id + i]
+                layer_arr.append((base_layer_obj_id + i, curr_layer_obj_id + i))
+                # print((base_layer_obj_id + i, curr_layer_obj_id + i))
+            base_layer_obj_id = curr_layer_obj_id
+            curr_layer_obj_id = layer_index_arr[layer]
+            
     DG = {}
     for goal_obj, goal_center in goal_arr.items():
-        DG[goal_obj] = set()
+        DG[goal_obj] = {}
         for start_obj, start_center in start_arr.items():
             if start_obj == goal_obj:
                 continue
             if (math.sqrt((start_center[0]-goal_center[0])**2 + (start_center[1]-goal_center[1])**2) <= 2*radius):
-                DG[goal_obj].add(start_obj)
+                DG[goal_obj][start_obj] = {}
+    # print(layer_arr)
+    for lower_obj, higher_obj in layer_arr:
+        DG[lower_obj][higher_obj] = {'layer': True}
     return DG
+
+
+def set2tuple(s):
+    '''
+    Args:
+    set: number set
+    
+    Return:
+    tuple: ordered number tuple
+    '''
+    return tuple(sorted(list(s)))
